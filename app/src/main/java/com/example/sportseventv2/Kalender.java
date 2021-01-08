5938ef698e0eeb4ec210126b10edac638a06c46f
mikkelwm <mikkelwm@gmail.com>
2021-01-08 18:15:07 +0100

    lækkerier med calender banditten

--- app/src/main/java/com/example/sportseventv2/Kalender.java
+++ app/src/main/java/com/example/sportseventv2/Kalender.java
@@ -1,36 +1,86 @@
 package com.example.sportseventv2;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.sportseventv2.model.EventAdapter;
-import com.example.sportseventv2.model.EventModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.FileAsyncHttpResponseHandler;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.WorkbookSettings;
+import jxl.read.biff.BiffException;
 
 public class Kalender extends TopMenu {
 
     RecyclerView recyclerView;
     EventAdapter eventAdapter;
+    AsyncHttpClient client;
+    Workbook workbook;
+    List<String> titles,descriptions,imageUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kalender);
+        //link til spreadsheet
+        String url = "https://github.com/NikolajMorgen/skedehans/blob/main/file.xls?raw=true";
 
-        // View til Kalenderen
-        recyclerView = findViewById(R.id.eventRecycler);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this)); // laver recycleren i linearLayout
-        eventAdapter = new EventAdapter(this,getMyList());
-        recyclerView.setAdapter(eventAdapter);
 
+        titles = new ArrayList<>();
+        descriptions = new ArrayList<>();
+        imageUrl = new ArrayList<>();
+
+        client = new AsyncHttpClient();
+        client.get(url, new FileAsyncHttpResponseHandler(this) {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, File file) {
+                Toast.makeText(Kalender.this, "Download Failed.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, File file) {
+                WorkbookSettings ws = new WorkbookSettings();
+                ws.setGCDisabled(true);
+                if(file != null) {
+                    try {
+                        workbook = workbook.getWorkbook(file);
+                        Sheet sheet = workbook.getSheet(0);
+                        for(int i = 0 ; i < sheet.getRows();i++){
+
+                            Cell[] row = sheet.getRow(i);
+                            titles.add(row[0].getContents());
+                            descriptions.add(row[1].getContents());
+                            imageUrl.add(row[2].getContents());
+
+                        }
+                        showData();
+                        Log.d("TAG", "onSuccess: "+ titles);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (BiffException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        });
 
 
         //initialiserer og tilknytter/tildeler variabler
@@ -64,30 +114,10 @@ public class Kalender extends TopMenu {
             }
         });
     }
-
-    // TODO Denne Metode laver indhold til Eventlisten
-    // Det er hardcoded nu, men skal laves dynamisk i forbindelse med databasen.
-    private ArrayList<EventModel> getMyList(){
-        ArrayList<EventModel> eventModels = new ArrayList<>();
-
-        EventModel m = new EventModel();
-        m.setEventTitle("Mongol Ræs");
-        m.setEventText("Et ræs for Jacob & Nikolaj");
-        m.setImage(R.drawable.niko_jaco);
-        eventModels.add(m);
-
-        m = new EventModel();
-        m.setEventTitle("Abe Ræs");
-        m.setEventText("Et ræs for Jacob & Nikolaj");
-        m.setImage(R.drawable.niko_jaco);
-        eventModels.add(m);
-
-        m = new EventModel();
-        m.setEventTitle("Røv Ræs");
-        m.setEventText("Et ræs for Jacob & Nikolaj");
-        m.setImage(R.drawable.niko_jaco);
-        eventModels.add(m);
-
-        return eventModels;
+    private void showData(){
+        recyclerView = findViewById(R.id.eventRecycler);
+        eventAdapter = new EventAdapter(this, titles, descriptions, imageUrl);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this)); // laver recycleren i linearLayout
+        recyclerView.setAdapter(eventAdapter);
     }
 }
