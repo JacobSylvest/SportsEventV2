6712bf4c290bd441f0d73916c11181ad4fa9fb29
jacob <jsylvest@hotmail.dk>
2021-01-10 11:42:07 +0100

    return true; design på profil_wButtons færdig

--- app/src/main/java/com/example/sportseventv2/RedigerProfil.java
+++ app/src/main/java/com/example/sportseventv2/RedigerProfil.java
@@ -1,8 +1,13 @@
 package com.example.sportseventv2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class RedigerProfil extends AppCompatActivity {
 
@@ -10,5 +15,37 @@ public class RedigerProfil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rediger_profil);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //sætter Løb som hjemmeskærm:
+
+        bottomNavigationView.setSelectedItemId(R.id.profil);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.kalender:
+                        startActivity(new Intent(getApplicationContext()
+                                , Kalender.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.løb:
+                        startActivity(new Intent(getApplicationContext()
+                                , Løb.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.profil:
+                        startActivity(new Intent(getApplicationContext()
+                        , Profil.class));
+                        return true;
+
+                }
+                return false;
+            }
+        });
     }
 }
