541f7bfa117150913bd7c758c4c2998398aa7a58
NikolajMorgen <s170182@student.dtu.dk>
2021-01-11 18:09:13 +0100

    Oprydning i klasser og samling af top og bund navigation i samme klasse "TopBundMenu"

--- app/src/main/java/com/example/sportseventv2/Profil.java
+++ app/src/main/java/com/example/sportseventv2/Profil.java
@@ -1,8 +1,5 @@
 package com.example.sportseventv2;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -10,11 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.security.PrivateKey;
-
-public class Profil extends TopMenu {
+public class Profil extends TopBundMenu {
 
     TextView fullName, username;
 
@@ -30,39 +27,6 @@ public class Profil extends TopMenu {
             @Override
             public void onClick(View v) {
                 openRedigerProfil();
-
-            }
-        });
-
-        //initialiserer og tilknytter/tildeler variabler
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        //sætter Løb som hjemmeskærm:
-
-        bottomNavigationView.setSelectedItemId(R.id.profil);
-
-        //Laver itemSelectedListener
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.kalender:
-                        startActivity(new Intent(getApplicationContext()
-                                , Kalender.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-
-                    case R.id.løb:
-                        startActivity(new Intent(getApplicationContext()
-                                , Løb.class));
-                        overridePendingTransition(0, 0);
-
-                    case R.id.profil:
-                        return true;
-
-                }
-                return false;
             }
         });
 
@@ -71,7 +35,7 @@ public class Profil extends TopMenu {
         username = findViewById(R.id.user_name);
 
         showAllUserData();
-
+        showNavProfil();
     }
 
     private void showAllUserData() {
@@ -81,9 +45,6 @@ public class Profil extends TopMenu {
 
         fullName.setText(user_name);
         username.setText(user_username);
-
-
-
     }
 
     public void openRedigerProfil(){
--- app/src/main/java/com/example/sportseventv2/TopKontakt.java
+++ app/src/main/java/com/example/sportseventv2/TopKontakt.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class TopKontakt extends TopMenu{
+public class TopKontakt extends TopBundMenu {
 
     private WebView webView;
 
